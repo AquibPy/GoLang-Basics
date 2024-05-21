a3f32af9a7271d71566c2dbf7cc2add8f5f9a0b3
AquibPy <mohdaquib31@gmail.com>
2024-05-21 13:23:58 +0530

    ADDED: Delete one and many & getallrecord from mongodb

--- 25mongodb/controller/controller.go
+++ 25mongodb/controller/controller.go
@@ -55,3 +55,44 @@ func updateOneMovie(movieID string) {
 
 	fmt.Println("modified count :", result.ModifiedCount)
 }
+
+func deleteOneMovie(movieID string) {
+	id, _ := primitive.ObjectIDFromHex(movieID)
+	filter := bson.M{"_id": id}
+	result, err := collection.DeleteOne(context.Background(), filter)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Movie got deleted with delete count :", result.DeletedCount)
+}
+
+func deleteAllMovies() int64 {
+	result, err := collection.DeleteMany(context.Background(), bson.D{{}})
+	if err != nil {
+		log.Fatal(err)
+	}
+	// fmt.Println("All Records Deleted Successfully",result.DeletedCount)
+	return result.DeletedCount
+
+}
+
+// get all movies from db
+
+func getAllMovies() []primitive.M {
+	cursor, err := collection.Find(context.Background(), bson.D{{}})
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer cursor.Close(context.Background())
+
+	var movies []primitive.M
+	for cursor.Next(context.Background()) {
+		var movie bson.M
+		err := cursor.Decode(&movie)
+		if err != nil {
+			log.Fatal(err)
+		}
+		movies = append(movies, movie)
+	}
+	return movies
+}
